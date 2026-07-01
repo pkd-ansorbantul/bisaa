db6cb0ac1ca78fd09ceca07205caa6d6dbb0e696
pkd-ansorbantul <ansor.bantulpkd@gmail.com>
2026-07-01 15:26:08 +0700

    Add files via upload

--- admin/api.js
+++ admin/api.js
@@ -8,7 +8,7 @@
   // ======================== KONFIGURASI ========================
   // Ganti dengan URL Apps Script Anda yang benar
   // Anda bisa menimpa nilai ini dengan variabel global PKD_SCRIPT_URL
-  const SCRIPT_URL_DEFAULT = 'https://script.google.com/macros/s/AKfycbxNpaLz5sxJ8tb71VyDtw489ZPCrz1OpDPZJoDi4OjP0fe_eOaLjjTnD1tljqmN4d4nDA/exec';
+  const SCRIPT_URL_DEFAULT = 'https://script.google.com/macros/s/AKfycbwJ756SRqRKuk4Uro20fp9ONGScc3K8d1tEqTy7d79Vqou0eO6xE4_ECRaf9aHo2Gbm/exec';
   const SCRIPT_URL = global.PKD_SCRIPT_URL || SCRIPT_URL_DEFAULT;
 
   // ======================== STATE ========================
