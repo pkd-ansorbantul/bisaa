c056499348008a9eddde007fffee434fe8dd7de6
pkd-ansorbantul <ansor.bantulpkd@gmail.com>
2026-07-19 14:45:35 +0700

    Add files via upload

--- js/core/config.js
+++ js/core/config.js
@@ -1,7 +1,7 @@
 // js/core/config.js
 // Versi Akhir - Konfigurasi Aplikasi
 // URL APPS SCRIPT SUDAH TERBUKTI BERHASIL DARI SCREENSHOT SEBELUMNYA
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxioQYWkdC96mUlYNGp42an1TdM3LT4gnR8G5NvQu4ZMQmgq6m4KvHrLjcFdCuyBJllDA/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxD3kXhcvqFjCaxnJ-JsBnIBcdftkD6Ttt1T2vrOWx-N0g4FTsDwr1Qoc2nxgR2WNJ5Q/exec';
 export const APP_NAME = 'PKD GP Ansor Kabupaten Bantul';
 export const APP_VERSION = '8.0.0';
 export const DEFAULT_CACHE_AGE = 10;
